5af306d007fbe79a3177cbc8da1291ba2aa95fad
Émeryk Bélisle <emeryk.belisle@hotmail.com>
2025-10-11 19:17:23 -0400

    Site Web finit
    Site Web finit
    Manque quelques retouches et les vrais données

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,10 +1,44 @@
 const app = Vue.createApp({
   data() {
     return {
+        isMenuOpen: false,
+        selectedCategory: "Tout",
+        searchResult: "",
+        students: [],
     }
   },
   methods: {
-  }
+    toggleBurgerMenu() {
+      this.isMenuOpen = !this.isMenuOpen;
+    },
+    closeBurgerMenu() {
+      this.isMenuOpen = false;
+    },
+  },
+  mounted() {
+    fetch('./src/js/students.json')
+    .then(response => response.json())
+    .then(data => {
+    this.students = data;
+    })
+    .catch(error => {
+    console.error('Les données des étudiants ne sont pas disponibles:', error);
+    });
+}
 });
 
 app.mount('#app');
+
+let lastScrollY = window.scrollY;
+const header = document.querySelector('header');
+const burgerMenu = document.querySelector(".wrapper-burger-menu")
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > lastScrollY) {
+    header.classList.add('hide-on-scroll');
+    app._instance.data.isMenuOpen = false;
+  } else {
+    header.classList.remove('hide-on-scroll');
+  }
+  lastScrollY = window.scrollY;
+});
